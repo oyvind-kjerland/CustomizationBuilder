2329313d6a9f35e46a0d2d0f2101bb57f4e575f2
Øyvind <oyvindk_93@hotmail.com>
2014-03-29 23:49:38 +0100

    Add TeamConfig

---
+++ CustomizationBuilder/src/config/TeamConfig.java
@@ -0,0 +1,18 @@
+package config;
+
+import java.io.Serializable;
+
+public class TeamConfig implements Serializable {
+
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+}
