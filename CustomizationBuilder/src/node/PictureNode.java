b18e694ad4898ed8285a843b1cfc516630ff079c
Øyvind <oyvindk_93@hotmail.com>
2014-03-29 21:58:22 +0100

    Add PictureNode

---
+++ CustomizationBuilder/src/node/PictureNode.java
@@ -0,0 +1,13 @@
+package node;
+
+public class PictureNode extends VisibleNode {
+
+	protected String texture;
+	
+	public String getTexture() {
+		return texture;
+	}
+	public void setTexture(String texture) {
+		this.texture = texture;
+	}
+}
