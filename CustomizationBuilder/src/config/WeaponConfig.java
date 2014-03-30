3d3fa7f59eee890be3f27a4a59c5eb3c8fa2f8d4
Øyvind <oyvindk_93@hotmail.com>
2014-03-30 03:11:19 +0200

    Add WeaponConfig

---
+++ CustomizationBuilder/src/config/WeaponConfig.java
@@ -0,0 +1,14 @@
+package config;
+
+public class WeaponConfig {
+
+	
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
