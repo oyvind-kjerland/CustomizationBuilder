9ee05ef6475d213a5f2983c69a8fbed663720dbe
Øyvind <oyvindk_93@hotmail.com>
2014-03-30 03:25:04 +0200

    Add CustomizationBuilder

---
+++ CustomizationBuilder/src/builder/CustomizationBuilder.java
@@ -0,0 +1,22 @@
+package builder;
+
+import gui.MainFrame;
+
+public class CustomizationBuilder {
+
+	
+	public CustomizationBuilder() {
+		MainFrame mainFrame = new MainFrame(this);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		CustomizationBuilder cutomizationBuilder = new CustomizationBuilder();
+	}
+}
