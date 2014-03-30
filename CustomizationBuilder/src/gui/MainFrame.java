1729180c63b59a716a69da141ab23e4659f14c39
Øyvind <oyvindk_93@hotmail.com>
2014-03-30 03:25:24 +0200

    Add MainFrame

---
+++ CustomizationBuilder/src/gui/MainFrame.java
@@ -0,0 +1,30 @@
+package gui;
+
+import javax.swing.JFrame;
+
+import builder.CustomizationBuilder;
+
+public class MainFrame extends JFrame {
+
+	private CustomizationBuilder customizationBuilder;
+	
+	public MainFrame(CustomizationBuilder customizationBuilder) {
+		
+		this.customizationBuilder = customizationBuilder;
+		
+		this.setTitle("CustomizationBuilder");
+		this.setVisible(true);
+		showMainPanel();
+		
+	}
+	
+	public void showMainPanel() {
+		
+		MainPanel mainPanel = new MainPanel(customizationBuilder);
+		this.add(mainPanel);
+		this.pack();
+		//this.revalidate();
+		
+	}
+	
+}
