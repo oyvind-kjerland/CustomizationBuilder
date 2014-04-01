f547523ef69e61f0b37bea77e3352d7576467d57
Øyvind <oyvindk_93@hotmail.com>
2014-04-01 17:07:03 +0200

    Use MenuBar. Tab header fix.

--- CustomizationBuilder/src/configGUI/ConfigPanel.java
+++ CustomizationBuilder/src/configGUI/ConfigPanel.java
@@ -1,10 +1,15 @@
 package configGUI;
 
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 
 import builder.CustomizationBuilder;
@@ -13,22 +18,121 @@ import gui.MainFrame;
 
 public class ConfigPanel extends GenericPanel {
 
+	
+	private static final Dimension panelSize = new Dimension(500, 300);
+	
 	public ConfigPanel(CustomizationBuilder customizationBuilder, MainFrame mainFrame) {
 		super(customizationBuilder, mainFrame);
 		
-		JButton backButton = new JButton("Back");
-		backButton.addActionListener(new ActionListener() {
+		makeMenuBar();
+		makeTabs();
+
+	}
+	
+	private void makeMenuBar() {
+		JMenuBar menuBar = new JMenuBar();
+		
+		JMenu fileMenu = new JMenu("File");
+		
+		JMenuItem menuItem;
+		
+		menuItem = new JMenuItem("New");
+		menuItem.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				showMainPanel();
+				newConfig();
 			}
 		});
-		//add(backButton);
+		fileMenu.add(menuItem);
 		
-		JTabbedPane tabbedPane = new JTabbedPane();
-		add(tabbedPane);
-		tabbedPane.addTab("BackTab", backButton);
-		tabbedPane.addTab("Test", new JLabel("Heh this is some pretty long label."));
+		
+		menuItem = new JMenuItem("Open...");
+		menuItem.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				openConfig();
+			}
+		});
+		fileMenu.add(menuItem);
+		
+		
+		menuItem = new JMenuItem("Save");
+		menuItem.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				saveConfig();
+			}
+		});
+		fileMenu.add(menuItem);
+		
+		menuItem = new JMenuItem("Save as...");
+		menuItem.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				saveConfigAs();
+			}
+		});
+		fileMenu.add(menuItem);
+		
+		
+		menuBar.add(fileMenu);
+		mainFrame.setJMenuBar(menuBar);
+	}
+	
+	private void makeTabs() {
+		JTabbedPane tabs = new JTabbedPane();
+		this.add(tabs);
+		
+		
+		JPanel tabPanel;
+		
+		// General tab
+		tabPanel = new JPanel();
+		tabPanel.setPreferredSize(panelSize);
+		tabs.addTab(formatTab("General"), tabPanel);
+		
+		// Team tab
+		tabPanel = new JPanel();
+		tabPanel.setPreferredSize(panelSize);
+		tabs.addTab(formatTab("Team"), tabPanel);
+		
+		// Kit tab
+		tabPanel = new JPanel();
+		tabPanel.setPreferredSize(panelSize);
+		tabs.addTab(formatTab("Kit"), tabPanel);
+		
+		// Weapon tab
+		tabPanel = new JPanel();
+		tabPanel.setPreferredSize(panelSize);
+		tabs.addTab(formatTab("Weapon"), tabPanel);
+		
+		// Part tab
+		tabPanel = new JPanel();
+		tabPanel.setPreferredSize(panelSize);
+		tabs.addTab(formatTab("Part"), tabPanel);
+	}
+	
+	private String formatTab(String name) {
+		return "<html><body><table width='50'>"+name+"</table></body></html>";
+	}
+	
+	
+	
+	
+	private void newConfig() {
+		
+	}
+	
+	private void openConfig() {
+		
+	}
+	
+	private void saveConfig() {
+		
+	}
+	
+	private void saveConfigAs() {
+		
 	}
 	
 	private void showMainPanel() {
